ac4e4f25fff379a5122106b3b238981d46fd7a6e
Kuznetsov_Yegor <kuznetsovyegor25@gmail.com>
2023-09-29 20:26:07 +0300

    [feat] add findRepetativePhones interview task

---
+++ src/algos/arrays/repetative-phone-numbers/index.ts
@@ -0,0 +1,27 @@
+interface PersonInfo {
+    name: string;
+    phones: number[];
+}
+type Notebook = PersonInfo[];
+
+export const findRepetativePhones = (notebook: Notebook) => {
+    const phonesInfo = new Map<number, string[]>();
+
+    for (const personInfo of notebook) {
+        const { name } = personInfo;
+
+        for (const phone of personInfo.phones) {
+            if (!phonesInfo.has(phone)) {
+                phonesInfo.set(phone, []);
+            }
+
+            const phoneInfo = phonesInfo.get(phone)!;
+            phoneInfo.push(name);
+        }
+    }
+
+    return [...phonesInfo]
+        .filter(([, phones]) => phones.length > 1)
+        .map(([phone, names]) => `${names.join(', ')}: ${phone}`)
+        .join('\n');
+};
