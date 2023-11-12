45ca62bfc56b66388d54e73e4cb33863695086ee
Kuznetsov_Yegor <kuznetsovyegor25@gmail.com>
2023-11-12 14:28:02 +0300

    [feat] add BSTNode class

---
+++ src/algos/data-structures/BST-node/index.ts
@@ -0,0 +1,51 @@
+export class BSTNode {
+    val: number;
+    left: BSTNode | null;
+    right: BSTNode | null;
+
+    constructor(val: number, left?: BSTNode, right?: BSTNode) {
+        this.val = val;
+        this.left = left !== undefined ? left : null;
+        this.right = right !== undefined ? right : null;
+        const isValid = this.performSelfValidate();
+
+        if (!isValid) {
+            throw new Error(
+                'BST is not valid, all nodes to the left must have lesser values and all nodes to the right - greater values'
+            );
+        }
+    }
+
+    public performSelfValidate(): boolean {
+        const root = this;
+
+        if (root === null) {
+            return true;
+        }
+
+        const stack: BSTNode[] = [root];
+
+        while (stack.length > 0) {
+            const node = stack.pop()!;
+            const nodeValue = node.val;
+            const leftValue = node.left?.val;
+            const rightValue = node.right?.val;
+
+            if (node.left !== null && leftValue !== undefined) {
+                if (nodeValue <= leftValue) {
+                    return false;
+                }
+                stack.push(node.left);
+            }
+
+            if (node.right !== null && rightValue !== undefined) {
+                if (nodeValue <= rightValue) {
+                    return false;
+                }
+                stack.push(node.right);
+            }
+        }
+
+        return true;
+    }
+}
