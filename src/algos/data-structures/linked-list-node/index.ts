6def2a7ddc12a6b561c5588929cffc7ee20c0f57
Kuznetsov Yegor <kuznetsovyegor25@gmail.com>
2024-06-05 23:44:30 +0300

    [feat] add fromArray and toArray methods to LinkListNode

--- src/algos/data-structures/linked-list-node/index.ts
+++ src/algos/data-structures/linked-list-node/index.ts
@@ -6,4 +6,37 @@ export class LinkedListNode<GValue> {
         this.value = value;
         this.next = next ?? null;
     }
+
+    public static fromArray<GValue>(items: GValue[]): LinkedListNode<GValue> {
+        if (items.length === 0) {
+            throw new Error('Input array must not be empty');
+        }
+
+        const head = new LinkedListNode(items[0]);
+        let tail = head;
+
+        for (let index = 1; index < items.length; index++) {
+            const newNode = new LinkedListNode(items[index]);
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        return head;
+    }
+
+    public toArray(): GValue[] {
+        const result: GValue[] = [];
+
+        const stack: (LinkedListNode<GValue> | null)[] = [this];
+        while (stack.length > 0) {
+            const node = stack.pop();
+            if (!node) {
+                continue;
+            }
+            result.push(node.value);
+            stack.push(node.next);
+        }
+
+        return result;
+    }
 }
