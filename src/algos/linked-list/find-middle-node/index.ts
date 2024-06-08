9668f24a3012344ed077915c0b1c47e40d333d3f
Kuznetsov Yegor <kuznetsovyegor25@gmail.com>
2024-06-08 03:10:44 +0300

    [feat] add find-middle-node optimized solution

--- src/algos/linked-list/find-middle-node/index.ts
+++ src/algos/linked-list/find-middle-node/index.ts
@@ -11,3 +11,25 @@ export const findMiddleNode = <GValue>(head: LinkedListNode<GValue>) => {
 
     return nodesArray[Math.round((nodesArray.length - 1) / 2)];
 };
+
+export const findMiddleNodeOptimized = <GValue>(head: LinkedListNode<GValue>) => {
+    let nodesCount = 0;
+    let currentNode: LinkedListNode<GValue> | null = head;
+
+    while (currentNode !== null) {
+        nodesCount++;
+        currentNode = currentNode.next;
+    }
+
+    const middleIndex = Math.floor(nodesCount / 2);
+    let currentIndex = 0;
+    currentNode = head;
+
+    while (currentNode !== null) {
+        if (currentIndex === middleIndex) {
+            return currentNode;
+        }
+        currentIndex++;
+        currentNode = currentNode.next;
+    }
+};
--- src/algos/linked-list/find-middle-node/middle-node.test.ts
+++ src/algos/linked-list/find-middle-node/middle-node.test.ts
@@ -1,30 +1,60 @@
 import { LinkedListNode } from '$algos/data-structures/linked-list-node';
-import { findMiddleNode } from '.';
+import { findMiddleNode, findMiddleNodeOptimized } from '.';
 
 describe('find middle node tests', () => {
-    test('should work with single node', () => {
-        const head = new LinkedListNode(1);
-        expect(findMiddleNode(head)).toBe(head);
+    describe('base solution', () => {
+        test('should work with single node', () => {
+            const head = new LinkedListNode(1);
+            expect(findMiddleNode(head)).toBe(head);
+        });
+
+        test('should work with odd nodes count', () => {
+            const head = new LinkedListNode(1);
+            head.next = new LinkedListNode(2);
+            const middle = new LinkedListNode(3);
+            head.next.next = middle;
+            middle.next = new LinkedListNode(4);
+            middle.next.next = new LinkedListNode(5);
+
+            expect(findMiddleNode(head)).toBe(middle);
+        });
+
+        test('should work with even nodes count', () => {
+            const head = new LinkedListNode(1);
+            head.next = new LinkedListNode(2);
+            const middleRight = new LinkedListNode(3);
+            head.next.next = middleRight;
+            middleRight.next = new LinkedListNode(4);
+
+            expect(findMiddleNode(head)).toBe(middleRight);
+        });
     });
 
-    test('should work with odd nodes count', () => {
-        const head = new LinkedListNode(1);
-        head.next = new LinkedListNode(2);
-        const middle = new LinkedListNode(3);
-        head.next.next = middle;
-        middle.next = new LinkedListNode(4);
-        middle.next.next = new LinkedListNode(5);
+    describe('optimized solution', () => {
+        test('should work with single node', () => {
+            const head = new LinkedListNode(1);
+            expect(findMiddleNodeOptimized(head)).toBe(head);
+        });
 
-        expect(findMiddleNode(head)).toBe(middle);
-    });
+        test('should work with odd nodes count', () => {
+            const head = new LinkedListNode(1);
+            head.next = new LinkedListNode(2);
+            const middle = new LinkedListNode(3);
+            head.next.next = middle;
+            middle.next = new LinkedListNode(4);
+            middle.next.next = new LinkedListNode(5);
 
-    test('should work with even nodes count', () => {
-        const head = new LinkedListNode(1);
-        head.next = new LinkedListNode(2);
-        const middleRight = new LinkedListNode(3);
-        head.next.next = middleRight;
-        middleRight.next = new LinkedListNode(4);
+            expect(findMiddleNodeOptimized(head)).toBe(middle);
+        });
 
-        expect(findMiddleNode(head)).toBe(middleRight);
+        test('should work with even nodes count', () => {
+            const head = new LinkedListNode(1);
+            head.next = new LinkedListNode(2);
+            const middleRight = new LinkedListNode(3);
+            head.next.next = middleRight;
+            middleRight.next = new LinkedListNode(4);
+
+            expect(findMiddleNodeOptimized(head)).toBe(middleRight);
+        });
     });
 });
